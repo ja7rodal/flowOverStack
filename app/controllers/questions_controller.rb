9a5039fe5247b01029d66b703bed835b51e9f89e
Jasson Rdz <ja7rodal@gmail.com>
2016-11-08 15:54:49 -0500

    comtroller crud questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,46 @@
 class QuestionsController < ApplicationController
   def index
+    @question = Question.all
   end
 
   def show
   end
 
   def new
+    @question = Question.new
   end
 
   def create
+    @question = Question.create(questions_params)
+    if @question.save
+      redirect_to root_path notice: "Pregunta publicada con exito"
+    else
+      render :new
+    end
   end
+
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    if @question.update(question_params)
+      redirect_to root_path, notice: "Pregunta actualizada correctamente"
+    else
+      render :edit
+    end
+  end
+
+  private
+  def questions_params
+    params.require(:question).permit(:title, :description)
+  end
+
+
 end
