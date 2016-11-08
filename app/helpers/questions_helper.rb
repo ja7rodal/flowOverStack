9ea2e0af2dce4a7f32d87e14ad88d602e4d566b5
Jasson Rdz <ja7rodal@gmail.com>
2016-11-08 16:23:59 -0500

    add helper for form new-update

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,9 @@
 module QuestionsHelper
+  def form_title
+    @question.new_record? ? "Nueva Pregunta" : "Modificar Pregunta"
+  end
+
+  def form_submit
+    @question.new_record? ? "Enviar Pregunta" : "Modificar la pregunta "
+  end
 end
