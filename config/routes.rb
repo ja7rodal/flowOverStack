be5127aaf65369a31f82370b71c0cd5c7905c085
Jasson Rdz <ja7rodal@gmail.com>
2016-11-08 15:36:23 -0500

    model and controller for question

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'questions/index'
+
+  get 'questions/show'
+
+  get 'questions/new'
+
+  get 'questions/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
