7ce7660a3cd9a1988d1bb2b7d2e590ca38909245
Jasson Rdz <ja7rodal@gmail.com>
2016-11-08 22:29:24 -0500

    asscociate questions to user

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -13,6 +13,7 @@ class QuestionsController < ApplicationController
 
   def create
     @question = Question.create(questions_params)
+    @question.user = current_user
     if @question.save
       redirect_to root_path notice: "Pregunta publicada con exito"
     else
