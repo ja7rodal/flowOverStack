9e949bd3152749aa8e7f4be30a59e27985f06d81
Jasson Rdz <ja7rodal@gmail.com>
2016-11-08 23:00:07 -0500

    routes answer

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'questions#index'
-  resources :questions
+  resources :questions do
+    resource :answers, only: [:create]
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
