30a352d55ebc4f9ce62e6b45eedda912199ec340
Jasson Rdz <ja7rodal@gmail.com>
2016-11-10 20:55:32 -0500

    routes polymorphic-comments

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   devise_for :users
   root 'questions#index'
   resources :questions do
-    resource :answers, only: [:create]
+    resource :answers, only: [:create] do
+      resources :comments, only: [:create]
+    end
+
+    esources :comments, only: [:create]
+      
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
