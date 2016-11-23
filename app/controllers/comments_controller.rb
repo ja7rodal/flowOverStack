35bb465ae6bde04b2b484445b2e910d19e96c8f7
Jasson Rdz <ja7rodal@gmail.com>
2016-11-23 12:11:42 -0500

    coments_controller.rb into questions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,16 @@
 class CommentsController < ApplicationController
-  def index
-    @commentable = find_commentable
-    @comments = @commentable.comments
-  end
+  before_action :authenticate_user!
 
   def create
-    @commentable = find_commentable
-    @comment = @commentable.comments.build(params[:comment])
+    @comment = @commentable.comments.new comment_params
+    @comment.user = current_user
     @comment.save
+    redirect_to @commentable
   end
 
-  def find_commentable
-    params.each do |name,value|
-      if name =~ /(.+)_id$/
-        return $1.classify.constantize.find(value)
-      end
+  private
+
+    def comment_params
+      params.require(:comment).permit(:body)
     end
-  end
-  nil
 end
---
+++ app/controllers/questions/comments_controller.rb
@@ -0,0 +1,9 @@
+class Questions::CommentsController < CommentsController
+  before_action :set_commentable
+
+  private
+
+    def set_commentable
+      @commentable = Film.find(params[:film_id])
+    end
+end
