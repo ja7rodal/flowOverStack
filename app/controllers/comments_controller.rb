d71a0c8287cf24fe343eddc30752610fd1933f4c
Jasson Rdz <ja7rodal@gmail.com>
2016-11-23 18:13:32 -0500

    comments for answers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,12 +5,12 @@ class CommentsController < ApplicationController
     @comment = @commentable.comments.new comment_params
     @comment.user = current_user
     @comment.save
-    redirect_to @commentable
+    redirect_to :back
   end
 
   private
 
     def comment_params
-      params.require(:comment).permit(:body)
+      params.require(:comment).permit(:body).merge(user: current_user)
     end
 end
