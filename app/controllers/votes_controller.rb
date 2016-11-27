994397f4d05cc162059752c0daf7b62e49440be6
Jasson Rdz <ja7rodal@gmail.com>
2016-11-27 13:58:31 -0500

    delete votes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,4 +1,30 @@
 class VotesController < ApplicationController
+  before_action :authenticate_user!
+  #before_filter :load_voteable
+
   def create
+    resource, id = request.path.split('/')[1, 2]
+    @voteable = resource.singularize.classify.constantize.find(id)
+    @voteable.class.to_s == "Answer" ? @question = @voteable.question : @question = @voteable
+    @vote = @voteable.votes.create(user: current_user)
+    @answer = Answer.new
+    @comment = Comment.new
+    redirect_to @question, notice: "Voto Creado"
+
+  end
+
+  def destroy
+    @question = Question.find(params[:ans])
+    @vote = Vote.find(params[:id])
+    @vote.destroy
+    redirect_to @question, notice: "Voto eliminado"
+  end
+
+  private
+
+  def load_voteable
+    resource, id = request.path.split('/')[1, 2]
+    @voteable = resource.singularize.classify.constantize.find(id)
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,24 @@
 Rails.application.routes.draw do
 
-  get 'votes/create'
+
 
   devise_for :users
   root 'questions#index'
 
   resources :questions, shallow: true do
     resources :comments, only: [:create]
+    resources :votes, only: [:create, :destroy]
+
     resources :answers, only: [:create],shallow: true do
-        resources :comments, only: [:create]
+      resources :votes, only: [:create, :destroy]
+      resources :comments, only: [:create]
     end
 
   end
 
   get 'questions/:id/answers', to: 'questions#show'
   get 'questions/:id/comments', to: 'questions#show'
+  get 'questions/:id/votes', to: 'questions#show'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
